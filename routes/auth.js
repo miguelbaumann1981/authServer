ebced013541108966ee76fd211eaf969ae528ffb
MiguelMartinezBBVA <miguel.martinez3.contractor@bbva.com>
2022-10-07 13:35:25 +0200

    avance viernes

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+
+const { Router } = require('express');
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+
+const router = Router();
+
+// Crear un nuevo usuario
+router.post('/new', crearUsuario);
+
+// Login usuario
+router.post('/', loginUsuario);
+
+// Validar y revalidar token
+router.get('/renew', revalidarToken);
+
+
+module.exports = router;
